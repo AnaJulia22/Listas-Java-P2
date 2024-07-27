6bf352b9532a4f8f877fa4c02f5b0e33ab29ca31
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-07-27 17:36:40 -0300

    lista 1

---
+++ Lista1/AumentoSalarial.java
@@ -0,0 +1,30 @@
+package Lista1;
+import java.util.Scanner;
+
+public class AumentoSalarial {
+    /*(3) Faça um programa que receba o salário de um funcionário, calcule e mostre o novo salário, sabendo-se
+              que este sofreu um aumento de 25%.
+
+        * Exemplo de execução – Exercício 3 - Caso de teste
+        Insira o salário:
+        1000
+        Novo salário:  1250*/
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double salario, aumento;
+
+        System.out.println("Digite seu salario:");
+
+        salario = scanner.nextDouble();
+
+        aumento = salario * 1.25;
+
+        System.out.printf("Resultado: %.2f", aumento);
+
+        scanner.close();
+
+    }
+}
---
+++ Lista1/Soma.java
@@ -0,0 +1,33 @@
+package Lista1;
+import java.util.Scanner;
+
+public class Soma {
+    /*(1) Faça um programa que receba quatro
+        números inteiros, calcule e mostre a soma desses números.
+
+        * Exemplo de execução – Exercício 1 - Caso de teste
+        Informe quatro números, em sequência:
+        1
+        2
+        3
+        4
+        Resultado da soma:  10*/
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n1, n2, n3, n4, soma;
+
+        System.out.println("Digite quatro números em sequência:");
+        n1 = scanner.nextInt();
+        n2 = scanner.nextInt();
+        n3 = scanner.nextInt();
+        n4 = scanner.nextInt();
+
+        soma = n1 + n2 + n3 + n4;
+
+        System.out.println("Resultado: " + soma);
+
+        scanner.close();
+    }
+}
